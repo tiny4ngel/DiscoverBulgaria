0e25349f2d892b59c2bd3184544d83504c290bc0
Julie Ivanova <66208546+tiny4ngel@users.noreply.github.com>
2023-08-11 16:02:17 +0300

    add tests

--- discoverBulgaria/bulgaria/tests.py
+++ discoverBulgaria/bulgaria/tests.py
@@ -1,3 +1,111 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from discoverBulgaria.bulgaria.models import Landmarks
+from discoverBulgaria.bulgaria.views import HistoricFiguresListView, LandmarksView, LandmarkDetailView
+from discoverBulgaria.users.models import AppUser, Profile
+
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.user = AppUser.objects.create_user(
+            email='testuser@example.com',
+            password='testpassword',
+        )
+        self.client.login(email=self.user.email, password='testpassword')
+        self.profile = Profile.objects.create(
+            first_name="Test",
+            last_name="User",
+            nationality="BG",
+            user=self.user,
+        )
+
+    def check_user_authenticated(self, response):
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def check_template_used(self, response, template_name):
+        self.assertTemplateUsed(response, template_name)
+
+
+class LandmarksGeneralTests(BaseTest):
+    url = '/bulgaria/dashboard/'
+    template_name = 'pages/landmarks.html'
+    view_name = LandmarksView.as_view().__name__
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.check_user_authenticated(response)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.check_template_used(response, self.template_name)
+
+    def test_view_connected_correctly(self):
+        match = resolve(self.url)
+        self.assertEqual(match.func.__name__, self.view_name)
+
+    def test_contains_in_html(self):
+        response = self.client.get(self.url)
+        self.check_user_authenticated(response)
+        self.assertInHTML('Landmarks', response.content.decode())
+
+
+class HistoricFiguresTests(BaseTest):
+    url = '/bulgaria/historic-figures/'
+    template_name = 'pages/historic_figures.html'
+    view_name = HistoricFiguresListView.as_view().__name__
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.check_user_authenticated(response)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.check_template_used(response, self.template_name)
+
+    def test_view_connected_correctly(self):
+        match = resolve(self.url)
+        self.assertEqual(match.func.__name__, self.view_name)
+
+    def test_contains_in_html(self):
+        response = self.client.get(self.url)
+        self.check_user_authenticated(response)
+        self.assertInHTML('Historic figures', response.content.decode())
+
+
+class LandmarkDetailTests(BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.landmark = Landmarks.objects.create(
+            title="Test Landmark",
+            location="Test Location",
+            landmark_photo=None,
+            trip_time="1 hour",
+            historic_context="Test historic context",
+            architectural_features="Test architectural features",
+            visitor_information="Test visitor information",
+            accessibility="Test accessibility information",
+            cover_photo=None,
+            additional_photo=None
+        )
+        self.url = reverse('landmark details', args=[self.landmark.pk])
+        self.template_name = 'pages/landmark_details.html'
+        self.view_name = LandmarkDetailView.as_view().__name__
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.check_user_authenticated(response)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.check_template_used(response, self.template_name)
+
+    def test_view_connected_correctly(self):
+        match = resolve(self.url)
+        self.assertEqual(match.func.__name__, self.view_name)
+
+    def test_contains_in_html(self):
+        response = self.client.get(self.url)
+        self.check_user_authenticated(response)
+        self.assertInHTML(self.landmark.title, response.content.decode())
--- discoverBulgaria/users/tests.py
+++ discoverBulgaria/users/tests.py
@@ -1,81 +1,25 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve
+from discoverBulgaria.users.views import index_no_account
 
-from discoverBulgaria.bulgaria.models import Landmarks
 
-
-class ViewsTestCase(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            email='testuser@example.com',
-            password='testpassword'
-        )
-        self.client.login(email=self.user.email, password='testpassword')
-        self.add_landmark_permission = Permission.objects.get(codename='add_landmarks')
-        self.user.user_permissions.add(self.add_landmark_permission)
-
-    def test_add_landmark_view(self):
-        response = self.client.get(reverse('add landmark'))
+class HomepageTests(TestCase):
+    def test_status_code(self):
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'pages/add_landmark.html')
 
+    def test_template_used(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
 
-def test_edit_landmark_view(self):
-    landmark = Landmarks.objects.create(title='Test Landmark', description='Description')
-    response = self.client.get(reverse('landmark edit', args=[landmark.pk]))
-    self.assertEqual(response.status_code, 200)
-    self.assertTemplateUsed(response, 'pages/landmark_edit.html')
+    def test_view_connected_correctly(self):
+        match = resolve('/')
 
+        self.assertEqual(match.func.__name__, index_no_account.__name__)
 
-def test_delete_landmark_view(self):
-    landmark = Landmarks.objects.create(title='Test Landmark', description='Description')
-    response = self.client.get(reverse('landmark delete', args=[landmark.pk]))
-    self.assertEqual(response.status_code, 200)
-    self.assertTemplateUsed(response, 'pages/landmarks.html')
+    def test_contains_in_html(self):
+        response = self.client.get('/')
 
+        self.assertEqual(response.status_code, 200)
 
-def test_delete_landmark_success(self):
-    landmark = Landmarks.objects.create(title='Test Landmark', description='Description')
-    response = self.client.post(reverse('landmark delete', args=[landmark.pk]))
-    self.assertEqual(response.status_code, 302)  # Redirect after successful form submission
-    self.assertEqual(Landmarks.objects.count(), 0)  # Ensure the object was deleted
-
-
-class AddLandmarkViewTest(TestCase):
-    def test_add_landmark(self):
-        data = {
-            'title': 'Test Landmark',
-            'location': 'Test Location',
-            'trip_time': 'Test Trip Time',
-            'historic_context': 'Test Historic Context',
-            'architectural_features': 'Test Architectural Features',
-            'visitor_information': 'Test Visitor Information',
-            'accessibility': 'Test Accessibility',
-        }
-        response = self.client.post(reverse('add landmark'), data)
-        self.assertEqual(response.status_code, 302)  # Redirect after successful form submission
-        self.assertEqual(Landmarks.objects.count(), 1)  # Ensure the object was created
-        new_landmark = Landmarks.objects.first()
-        self.assertEqual(new_landmark.title, 'Test Landmark')
-        self.assertEqual(new_landmark.location, 'Test Location')
-        self.assertEqual(new_landmark.trip_time, 'Test Trip Time')
-        self.assertEqual(new_landmark.historic_context, 'Test Historic Context')
-        self.assertEqual(new_landmark.architectural_features, 'Test Architectural Features')
-        self.assertEqual(new_landmark.visitor_information, 'Test Visitor Information')
-        self.assertEqual(new_landmark.accessibility, 'Test Accessibility')
-
-    def test_add_landmark_invalid_data(self):
-        data = {
-            'title': 43,
-            'location': 44444,
-            'trip_time': 55,
-            'historic_context': 43342,
-            'architectural_features': 423234,
-            'visitor_information': True,
-            'accessibility': False,
-        }
-        response = self.client.post(reverse('add landmark'), data)
-        self.assertEqual(response.status_code, 200)  # Form submission should fail, returning to the same page
-        self.assertEqual(Landmarks.objects.count(), 0)  # No new objects should be created
+        self.assertInHTML('A Brief Historical Overview', response.content.decode())
